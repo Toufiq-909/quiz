fa444a4a81a1b575a704c8054788654973bc0d20
Toufiq-909 <muheeth312@gmail.com>
2025-02-20 20:26:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let index=0;
+setInterval(()=>{
+    let it=document.getElementById("hi");
+    it.style.opacity=0;
+    setTimeout(()=>{
+        let img=["Blogging-bro.png","Blogging-cuate.png","Blogging-pana.png"];
+        let it=document.getElementById("hi");
+        if(index==3)
+        {
+            index=0;
+        }
+        it.src=img[index];
+        it.style.opacity=1
+        index++;
+    },500)
+
+
+},5000)
+
